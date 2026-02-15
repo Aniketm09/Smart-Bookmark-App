a0d1b8e875f9b06f33371c47105518ded1e8f782
Aniket Mali <aniketmali@0912.com>
2026-02-15 23:04:44 +0530

    fixed oauth redirect

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,7 @@ export default function Login() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "http://localhost:3000",
+        redirectTo: window.location.origin,
       },
     });
   };
